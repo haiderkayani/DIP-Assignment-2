7c49a0763002378ffd2a8f653335805239e1458c
Haider <haidabid@gmail.com>
2023-10-24 17:44:53 +0500

    final commit
    removed test.py and finalized assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,31 +1,39 @@
 import cv2
 import numpy as np
 
-def label(image):
-    labeled_image = np.zeros_like(image)
-    neighbours = [(0,1), (1,0), (0,-1), (-1,0), (1,1), (-1,-1), (1,-1), (-1,1)]
-    
-    label = 1
-    
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            if image[i,j] == 255 and labeled_image[i,j] == 0:
-                stack = [(i,j)]
-                while stack:
-                    current_i, current_j = stack.pop()
-                    labeled_image[current_i, current_j] = label
-                    for x, y in neighbours:
-                        ni, nj = current_i+x, current_j+y
-                        if 0<=ni<image.shape[0] and 0<=nj<image.shape[1] and image[ni,nj] == 255 and labeled_image[ni,nj] == 0:
-                            stack.append((ni,nj))
-                            
-                label = label+1
-    return labeled_image
-              
-original_image = cv2.imread("Image_01.bmp", cv2.IMREAD_GRAYSCALE)
-labeled_image = label(original_image)
+image = cv2.imread("Image_01.bmp", 0)
+_, thresholded_image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
 
-cv2.imshow("Original image", original_image)
-cv2.imshow("Labeled objects image", labeled_image)
+def dfs(label, x, y):
+    stack = [(x,y)]
+    while stack:
+        i, j = stack.pop()
+        labels[i,j] = label
+
+        for dx in [-1,0,1]:
+            for dy in [-1,0,1]:
+                if dx==0 and dy==0:
+                    continue
+                new_x, new_y = i+dx, j+dy
+                if 0<=new_x<image.shape[0] and 0<=new_y<image.shape[1] and thresholded_image[new_x, new_y]==255 and labels[new_x, new_y]==0:
+                    stack.append((new_x, new_y))
+
+labels = np.zeros_like(thresholded_image)
+label = 1
+
+for i in range(thresholded_image.shape[0]):
+    for j in range(thresholded_image.shape[1]):
+        if thresholded_image[i, j] == 255 and labels[i, j] == 0:
+            dfs(label, i, j)
+            label = label+1
+
+output_image = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+colors = [tuple(np.random.choice(range(256), size=3)) for _ in range(label)]
+
+for i in range(1, label):
+    output_image[labels == i] = colors[i]
+
+cv2.imshow("original image", image)
+cv2.imshow("Labeled Image", output_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
